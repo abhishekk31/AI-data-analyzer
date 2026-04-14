960bafb8c86310f930a7494f10a333ea13ad006d
abhishekk31 <abhisheksasane212@gmail.co>
2026-04-14 20:01:12 +0530

    backend ready for render

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import Routes from './Routes.js'
 
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/',Routes)
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
